265b393a6fe6ad7016b7dc857b03446828df3080
FYTJ <69418038+FYTJ@users.noreply.github.com>
2024-12-21 18:58:12 +0800

    调整代码格式

--- main.c
+++ main.c
@@ -23,28 +23,30 @@ int main() {
     int mode;
     scanf("%d", &mode);
 
-    if (mode == 1) {
+    if (mode == 1)
         return human_human(game);
-    } else if (mode == 2) {
+    if (mode == 2) {
         printf("Choose black or white.\n1: black; 2: white\nchoose: ");
         int human_color;
         scanf("%d", &human_color);
         AI ai;
         AI_init(&ai, "ai", 2 - human_color);
         return human_ai(game, ai, human_color);
-    } else if (mode == 3) {
+    }
+    if (mode == 3) {
         AI ai1, ai2;
         AI_init(&ai1, "ai1", 1);
         AI_init(&ai2, "ai2", 2);
         return ai_ai(game, ai1, ai2);
-    } else if (mode == 4) {
+    }
+    if (mode == 4) {
         tune_parameters(game);
-    } else if (mode == 5) {
+    }
+    if (mode == 5) {
         int existed_chess[][2] = {
-
+            {7, 7}, {7, 8}, {8, 5}, {8, 9}, {6, 7}, {8, 7}, {7, 6}, {5, 8}, {9, 4}, {10, 3}, {8, 8}, {6, 9}
         };
         int num_existed = sizeof(existed_chess) / sizeof(existed_chess[0]);
-
         // 将预设棋子放置到棋盘上
         for (int i = 0; i < num_existed; i++) {
             int x = existed_chess[i][0];
@@ -52,20 +54,14 @@ int main() {
             Game_play(&game, x, y);
         }
 
-        // 初始化两个AI
         AI ai1, ai2;
         AI_init(&ai1, "ai1", 1);
         AI_init(&ai2, "ai2", 2);
-
-        // 显示棋盘，以最后一个棋子为标记
         int last_x = existed_chess[num_existed - 1][0];
         int last_y = existed_chess[num_existed - 1][1];
         Game_display_board(&game, last_x, last_y, 1);
-
-        // 让AI进行走子
         ai_play(&game, &ai1, 0, &last_x, &last_y);
     }
-
     return 0;
 }
 
@@ -73,17 +69,19 @@ int main() {
 int human_human(Game game) {
     Game_display_board(&game, -1, -1, 0);
     int x = 0, y = 0;
-    int winner = -1;
     while (Game_check_winner(game.board) == -1 && Game_check_ban(game.board, game.current_player, x, y) == -1) {
         human_play(&game, &x, &y);
-        if (Game_check_winner(game.board) != -1 && Game_check_ban(game.board, game.current_player, x, y) == -1) break;
+        if (Game_check_winner(game.board) != -1 && Game_check_ban(game.board, game.current_player, x, y) == -1)
+            break;
         human_play(&game, &x, &y);
     }
     for (int i = 0; i < game.record_count; i++) {
         printf("(%d,%d), ", game.record_chess[i][0], game.record_chess[i][1]);
     }
     printf("\n");
-    winner = Game_check_winner(game.board) != -1 ? Game_check_winner(game.board) : Game_check_ban(game.board, game.current_player, x, y);
+    int winner = Game_check_winner(game.board) != -1
+                     ? Game_check_winner(game.board)
+                     : Game_check_ban(game.board, game.current_player, x, y);
     return winner;
 }
 
@@ -92,20 +90,21 @@ int human_ai(Game game, AI ai, int human_color) {
     AI_read_openings(&ai, "/Users/zhuyanbo/PycharmProjects/人工智能：现代方法/gobang/openings.txt");
     Game_display_board(&game, -1, -1, 0);
     int x = 0, y = 0;
-    int winner = -1;
-    if (human_color == 2) {
+    if (human_color == 2)
         ai_play(&game, &ai, 1, &x, &y);
-    }
     while (Game_check_winner(game.board) == -1 && Game_check_ban(game.board, game.current_player, x, y) == -1) {
         human_play(&game, &x, &y);
-        if (Game_check_winner(game.board) != -1 && Game_check_ban(game.board, game.current_player, x, y) == -1) break;
+        if (Game_check_winner(game.board) != -1 && Game_check_ban(game.board, game.current_player, x, y) == -1)
+            break;
         ai_play(&game, &ai, 0, &x, &y);
     }
     for (int i = 0; i < game.record_count; i++) {
         printf("(%d,%d), ", game.record_chess[i][0], game.record_chess[i][1]);
     }
     printf("\n");
-    winner = Game_check_winner(game.board) != -1 ? Game_check_winner(game.board) : Game_check_ban(game.board, game.current_player, x, y);
+    int winner = Game_check_winner(game.board) != -1
+                 ? Game_check_winner(game.board)
+                 : Game_check_ban(game.board, game.current_player, x, y);
     return winner;
 }
 
@@ -115,18 +114,20 @@ int ai_ai(Game game, AI ai1, AI ai2) {
     AI_read_openings(&ai2, "/Users/zhuyanbo/PycharmProjects/人工智能：现代方法/gobang/openings.txt");
     Game_display_board(&game, -1, -1, 0);\
     int x = 0, y = 0;
-    int winner = -1;
     ai_play(&game, &ai1, 1, &x, &y);
     while (Game_check_winner(game.board) == -1) {
         ai_play(&game, &ai2, 0, &x, &y);
-        if (Game_check_winner(game.board) != -1) break;
+        if (Game_check_winner(game.board) != -1)
+            break;
         ai_play(&game, &ai1, 0, &x, &y);
     }
     for (int i = 0; i < game.record_count; i++) {
         printf("(%d,%d), ", game.record_chess[i][0], game.record_chess[i][1]);
     }
     printf("\n");
-    winner = Game_check_winner(game.board) != -1 ? Game_check_winner(game.board) : Game_check_ban(game.board, game.current_player, x, y);
+    int winner = Game_check_winner(game.board) != -1
+                 ? Game_check_winner(game.board)
+                 : Game_check_ban(game.board, game.current_player, x, y);
     for (int i = 0; i < game.record_count; i++) {
         printf("{%d,%d}, ", game.record_chess[i][0], game.record_chess[i][1]);
     }
@@ -139,9 +140,7 @@ void tune_parameters(Game game) {
     AI_init(&ai1, "ai1", 1);
     AI_init(&ai2, "ai2", 2);
 
-    // 假设用简单随机搜索测试参数
-    int best_weights[PATTERN_COUNT] = {50000,4320,720,720,120,50,20};
-    // 39729 3282 756 352 142 26 13
+    int best_weights[PATTERN_COUNT] = {50000, 4320, 720, 720, 120, 50, 20};
 
     for (int trial = 1; trial <= 1; trial++) {
         // 随机参数：例如在原值附近随机微调
@@ -151,7 +150,8 @@ void tune_parameters(Game game) {
             ai1.pattern_weights[i] = best_weights[i];
         }
         for (int i = 0; i < PATTERN_COUNT; i++) {
-            ai1.pattern_weights[i] = ai1.pattern_weights[i] + (rand() % ai1.pattern_weights[i] - ai1.pattern_weights[i] / 2);
+            ai1.pattern_weights[i] = ai1.pattern_weights[i] + (
+                                         rand() % ai1.pattern_weights[i] - ai1.pattern_weights[i] / 2);
             if (ai1.pattern_weights[i] < 0) ai1.pattern_weights[i] = 0; // 保持非负
         }
         // 对弈N局
