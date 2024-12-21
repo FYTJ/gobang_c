02f4fd073b0da9401300d7991f953ffd0fae43c6
FYTJ <69418038+FYTJ@users.noreply.github.com>
2024-12-21 18:59:35 +0800

    调整代码格式

--- ai.h
+++ ai.h
@@ -12,9 +12,6 @@
 typedef struct {
     char* name;
     int color;
-    int count;
-    int maxv;
-    int minv;
     int extension_threshold;
     double p_threshold;
     int lmr_threshold;
@@ -56,15 +53,19 @@ typedef struct {
 } Parallel_args;
 
 
-
 void AI_init(AI* ai, const char* name, int color);
 void AI_read_openings(AI* ai, const char* filename);
-int AI_heuristic_alpha_beta_search(AI* ai, Game* game, int depth, int is_first_move);
-void ai_play(Game* game, AI* ai, int first_move, int *x, int *y);
 int AI_look_up_table(AI* ai, int record_chess[225][2], int steps);
+int AI_heuristic_alpha_beta_search(AI* ai, Game* game, int depth, int is_first_move);
+static long long AI_eval_pos(AI* ai, int state[BOARD_SIZE][BOARD_SIZE], int player, int x, int y, int direction[2]);
+static void AI_get_line(int state[BOARD_SIZE][BOARD_SIZE], int player,int x,int y,int dx,int dy,char* line);
+static void* AI_parallel_max_value(void* arg);
+static long long AI_min_value(AI* ai, Game* game, int state[BOARD_SIZE][BOARD_SIZE], int player, double alpha, double beta,int depth);
+static long long AI_max_value(AI* ai, Game* game, int state[BOARD_SIZE][BOARD_SIZE], int player, double alpha, double beta,int depth);
+static long long AI_heuristic_eval(AI* ai, Game* game, int state[BOARD_SIZE][BOARD_SIZE], int player, int last_x, int last_y, int have_last, long long prev_eval, int prev_state[BOARD_SIZE][BOARD_SIZE]);
 
 int compare(const void *a, const void *b);
 int compare_reverse(const void *a, const void *b);
 void human_play(Game* game, int *x, int *y);
-
+void ai_play(Game* game, AI* ai, int first_move, int *x, int *y);
 #endif
