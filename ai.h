89d5858aa64e34cc1c484a64f6f9d9895ebe16e3
FYTJ <69418038+FYTJ@users.noreply.github.com>
2024-12-27 03:07:31 +0800

    Update ai.h

--- ai.h
+++ ai.h
@@ -40,7 +40,7 @@ typedef struct {
 typedef struct {
     int *task_index;
     AI* ai;
-    Game* game;
+    const Game* game;
     int **nstate_arg;
     int player;
     double alpha;
@@ -55,14 +55,14 @@ typedef struct {
 
 void AI_init(AI* ai, const char* name, int color);
 void AI_read_openings(AI* ai, const char* filename);
-int AI_look_up_table(AI* ai, int record_chess[225][2], int steps);
-int AI_heuristic_alpha_beta_search(AI* ai, Game* game, int depth, int is_first_move);
+int AI_look_up_table(AI* ai, const int record_chess[225][2], int steps);
+int AI_heuristic_alpha_beta_search(AI* ai, const Game* game, int depth, int is_first_move);
 static long long AI_eval_pos(AI* ai, int state[BOARD_SIZE][BOARD_SIZE], int player, int x, int y, int direction[2]);
 static void AI_get_line(int state[BOARD_SIZE][BOARD_SIZE], int player,int x,int y,int dx,int dy,char* line);
 static void* AI_parallel_max_value(void* arg);
-static long long AI_min_value(AI* ai, Game* game, int state[BOARD_SIZE][BOARD_SIZE], int player, double alpha, double beta,int depth);
-static long long AI_max_value(AI* ai, Game* game, int state[BOARD_SIZE][BOARD_SIZE], int player, double alpha, double beta,int depth);
-static long long AI_heuristic_eval(AI* ai, Game* game, int state[BOARD_SIZE][BOARD_SIZE], int player, int last_x, int last_y, int have_last, long long prev_eval, int prev_state[BOARD_SIZE][BOARD_SIZE]);
+static long long AI_min_value(AI* ai, const Game* game, int state[BOARD_SIZE][BOARD_SIZE], int player, double alpha, double beta,int depth);
+static long long AI_max_value(AI* ai, const Game* game, int state[BOARD_SIZE][BOARD_SIZE], int player, double alpha, double beta,int depth);
+static long long AI_heuristic_eval(AI* ai, const Game* game, int state[BOARD_SIZE][BOARD_SIZE], int player, int last_x, int last_y, int have_last, long long prev_eval, int prev_state[BOARD_SIZE][BOARD_SIZE]);
 
 int compare(const void *a, const void *b);
 int compare_reverse(const void *a, const void *b);
